1870c791456dd0c4d231001b955c421621c22e65
mmelihdogan <doganmmelih@gmail.com>
2020-02-19 18:49:17 +0300

    Done.

--- index.js
+++ index.js
@@ -4,12 +4,22 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(collection, callback) {
+          for (const i in collection) {
+            if (collection.hasOwnProperty(i)) {
+               callback(collection[i]);
+            }
+          }
+          return collection;
     },
 
-    map: function() {
-
+    map: function(collection, callback) {
+        let newCollection = [];
+        for(const i in collection) {
+          let element = callback(collection[i]);
+          {newCollection.push(element)};
+        }
+        return newCollection;
     },
 
     reduce: function() {
