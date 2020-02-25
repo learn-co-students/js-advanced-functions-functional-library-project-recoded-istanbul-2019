46792fa7847e6ede2a47ddc27ac78b72cd71d500
Abdulrahman Y. idlbi <adlogi@gmail.com>
2020-02-25 09:32:38 +0300

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const fi = (function() {
   return {
     libraryMethod: function() {
-      return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
+      return 'Start by reading the article!';
     },
 
-    each: function() {
-
+    each: function(collection, callback) {
+      for (const e of collection) {
+        callback(e);
+      }
+      return collection;
     },
 
     map: function() {
@@ -25,3 +28,5 @@ const fi = (function() {
 })()
 
 fi.libraryMethod()
+
+done();
