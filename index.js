75af9bc86d757e7f3f0a21f39c3ee2efff78e097
basil-kawak <basilalisa0@gmail.com>
2020-05-12 15:39:32 +0300

    Done.

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const fi = (function() {
             console.log(`${key}: ${input[key]}`);
              func(input[key])
             }
-        // newArr.push(Object.values(input))
+        // mArr.push(Object.values(input))
     }
     return input;
     },
@@ -35,11 +35,13 @@ const fi = (function() {
     },
 
     reduce: function(input, func, init) {
-      let acc = init === undefined ? 0 : init;
     for(let i=0; i<input.length; i++) {
-         acc = func(acc, input[i], input);
+      if (init === undefined) {
+        init = input[i]
+      }else
+         init = func(init, input[i], input);
     }
-    return acc;  
+    return init;  
     },
 
     find: function(input, func) {
@@ -91,32 +93,88 @@ const fi = (function() {
     },
 
     last: function(input, n) {
-      let result;
       if (!n) {
-          result=input.pop();
+        return input[input.length - 1]
       }else {
-        result = [];
-          for (let i = 0; i < input.length ; i++) {
-              console.log(input[i])
-              if (input[i] == n) {
-                  result.push(input[i]);
-                  break
-                  
-              }else {
-                  result.push(input[i])
-                  
-              }
-          }
-      }
-      return result
+        return input.slice(input.length - n, input.length)
+      } 
     },
 
-    functions: function() {
+    compact: function(input) {
+      let result = [];
+      for (let el of input) {
+        if (el) {
+          result.push(el);
+        }
+      }
+      return result;
+    },
 
+    sortBy: function(input, func) {
+      let result = [...input];
+      result.sort((x, y) => {
+        return func(x) - func(y);
+      });
+      return result;
+    },
+
+    flatten: function(array, shallow){
+      if(shallow){
+        return array.flat(1);
+      }
+     
+     return array.flat(Infinity);
+
+    },
+
+    uniq: function(array, isSorted, func){
+      let uniq = [];
+      let muArr = [];
+      let resuArr = [];
+      if (func) {
+        for (let i = 0; i < array.length; i++) {
+          muArr.push(func(array[i]))
+        }
+      } else {
+        muArr = [...array]
+      }
+
+      for (let i = 0; i < muArr.length; i++) {
+
+        if (uniq.indexOf(muArr[i]) === -1) {
+          uniq.push(muArr[i])
+          resuArr.push(array[i])
+        }
+      }
+
+      if (isSorted) {
+        return resuArr
+      } else {
+        return resuArr.sort((x, y) => x - y);
+      }
+  
+    },
+
+    keys: function(obj){
+      return Object.keys(obj)
+    },
+
+    values: function(obj){
+      return Object.values(obj)
+    },
+
+    functions: function(obj) {
+      let result=[]
+      for(let key in obj){
+        if(typeof(obj[key]) === 'function') {
+          result.push(key);
+        }
+      }
+      return result;
     },
 
 
   }
 })()
-
+console.log()
 fi.libraryMethod()
